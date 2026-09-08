ba7415349605f08cb2678b0ac44787048c1b1daf
joaquimbrasiliano <joaquimbrasilianog@gmail.com>
2026-09-08 08:36:06 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
 import java.util.LinkedList;
 import java.util.List;
 
-void main() {
-    ListaSimples l1 = new ListaSimples();
+private String linha(int qtde){
+    return "-".repeat(qtde);
+}
 
-    l1.inserirInicio(23);
+void main() throws Exception {
+    Scanner sc = new Scanner(System.in);
+
+    ListaSimples L1 = new ListaSimples();
+
+    linha(20);
+    System.out.println("Digite a quantidade de numeros a serem gerados: ");
+    int qtde = sc.nextInt();
+    linha(20);
+    for (int i = 0; i < qtde ; i++) {
+        Random random = new Random();
+        int gen = random.nextInt(101);
+
+        System.out.println("Onde deseja inserir o numero '" + gen + "': [1- Inicio, 2-Meio, 3-Fim]");
+        int pos = sc.nextInt();
+        switch (pos) {
+            case 1:
+                L1.inserirNoInicio(gen);
+                break;
+            case 2:
+                // L1.inserirNoMeio(gen, );
+                break;
+            case 3:
+                L1.inserirNoFim(gen);
+                break;
+            default:
+                throw new Exception("Opção inválida");
+        }
+
+    }
+
+    System.out.println("Lista 1: " + L1.exibir());
 }
