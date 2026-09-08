979740ba0bd8bfc8a12b09384d3dfc01135c7610
joaquimbrasiliano <joaquimbrasilianog@gmail.com>
2026-09-07 21:23:46 -0300

    Update ListaSimples.java

--- src/ListaSimples.java
+++ src/ListaSimples.java
@@ -1,30 +1,106 @@
 public class ListaSimples {
-    private No cabeca;
 
-    ListaSimples(){
-        this.cabeca = null;
+    protected No cabeca;
+
+    public ListaSimples() {
+        cabeca = null;
     }
 
-    public boolean ehVazia(){
-        return this.cabeca == null;
+    public void inserirNoInicio(int dado) {
+        No novo = new No(dado);
+
+        novo.setProximo(cabeca);
+        cabeca = novo;
+    }
+    
+    public void inserirNoFim(int dado) {
+        No novo = new No(dado);
+
+        if (cabeca == null) {
+            cabeca = novo;
+            return;
+        }
+
+        No atual = cabeca;
+
+        while (atual.getProximo() != null) {
+            atual = atual.getProximo();
+        }
+
+        atual.setProximo(novo);
     }
 
-    public void inserirInicio(int valor){
-        No aux = new No(valor);
+    public void inserirNoMeio(int dado, int posicao) {
+        if (posicao <= 0 || cabeca == null) {
+            inserirNoInicio(dado);
+            return;
+        }
 
-        if(ehVazia()){
-            cabeca.setProximo(aux);
-        } else {
-            aux.setProximo(cabeca);''
-            cabeca = aux;
+        No atual = cabeca;
+        int contador = 0;
+
+        while (atual.getProximo() != null &&
+               contador < posicao - 1) {
+
+            atual = atual.getProximo();
+            contador++;
+        }
+
+        No novo = new No(dado);
+
+        novo.setProximo(atual.getProximo());
+        atual.setProximo(novo);
+    }
+
+    public void removerNoInicio() {
+        if (cabeca != null) {
+            cabeca = cabeca.getProximo();
         }
     }
 
-    public No getCabeca() {
-        return cabeca;
+    public void removerNoFim() {
+        if (cabeca == null) {
+            return;
+        }
+
+        if (cabeca.getProximo() == null) {
+            cabeca = null;
+            return;
+        }
+
+        No atual = cabeca;
+
+        while (atual.getProximo().getProximo() != null) {
+            atual = atual.getProximo();
+        }
+
+        atual.setProximo(null);
     }
 
-    public void setCabeca(No cabeca) {
-        this.cabeca = cabeca;
+    public void removerNoMeio(int posicao) {
+        if (cabeca == null) {
+            return;
+        }
+
+        if (posicao <= 0) {
+            removerNoInicio();
+            return;
+        }
+
+        No atual = cabeca;
+        int contador = 0;
+
+        while (atual.getProximo() != null &&
+               contador < posicao - 1) {
+
+            atual = atual.getProximo();
+            contador++;
+        }
+
+        if (atual.getProximo() != null) {
+            atual.setProximo(
+                atual.getProximo().getProximo()
+            );
+        }
     }
 }
