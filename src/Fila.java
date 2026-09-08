2fea1e96f879ff5ecbb8c287db6edda7e4d78d14
joaquimbrasiliano <joaquimbrasilianog@gmail.com>
2026-09-07 21:24:17 -0300

    Create Fila.java

---
+++ src/Fila.java
@@ -0,0 +1,10 @@
+public class Fila extends ListaSimples {
+
+    public void inserir(int dado) {
+        inserirNoFim(dado);
+    }
+
+    public void remover() {
+        removerNoInicio();
+    }
+}
