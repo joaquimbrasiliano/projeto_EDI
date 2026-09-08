5248a349903cfbb01a7fab88548402f9c31f072e
joaquimbrasiliano <joaquimbrasilianog@gmail.com>
2026-09-07 21:24:35 -0300

    Create Deque.java

---
+++ src/Deque.java
@@ -0,0 +1,18 @@
+public class Deque extends ListaSimples {
+
+    public void inserirInicio(int dado) {
+        inserirNoInicio(dado);
+    }
+
+    public void inserirFim(int dado) {
+        inserirNoFim(dado);
+    }
+
+    public void removerInicio() {
+        removerNoInicio();
+    }
+
+    public void removerFim() {
+        removerNoFim();
+    }
+}
